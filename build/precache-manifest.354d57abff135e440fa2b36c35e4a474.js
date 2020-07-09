c214b44d541f3a04a48a961bf9050b16a5e9fc90
Truong vi <vinlt@antsprogrammatic.com>
2020-07-09 21:41:54 +0700

    Deploy

---
+++ build/precache-manifest.354d57abff135e440fa2b36c35e4a474.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94771b2b9c3405dfab08bbd43543d7f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31090938b4ef042c5ca3",
+    "url": "/static/css/2.ed4bc00d.chunk.css"
+  },
+  {
+    "revision": "89220081d22e175d2733",
+    "url": "/static/css/main.3a633f7e.chunk.css"
+  },
+  {
+    "revision": "31090938b4ef042c5ca3",
+    "url": "/static/js/2.78374822.chunk.js"
+  },
+  {
+    "revision": "270b97d916723fdd58a90bacd57e840d",
+    "url": "/static/js/2.78374822.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89220081d22e175d2733",
+    "url": "/static/js/main.7abb6aa6.chunk.js"
+  },
+  {
+    "revision": "64497e406bb8f70f2e18",
+    "url": "/static/js/runtime-main.f788f5b3.js"
+  },
+  {
+    "revision": "916241fbf6af341df24c61f2e8ec87d6",
+    "url": "/static/media/background.916241fb.jpg"
+  }
+]);
